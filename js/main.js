8bd12af77dbdb9cbabb8596cfb7fa447f2d97b50
Linus <linusstrangstad@gmail.com>
2020-04-03 00:28:45 +0200

    fetched products, looped and template literals

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+const myUrl =
+  "https://www.flower-power.bystrangstad.com/wp-json/wc/store/products/";
 
+fetch(myUrl)
+  .then(Response => Response.json())
+  .then(json => flowerInfo(json))
+  .catch(error => console.log(error));
+
+function flowerInfo(json) {
+  let data = json;
+
+  console.log(data);
+
+  const flowerContainer = document.querySelector(".row");
+
+  let html = "";
+
+  data.forEach(result => {
+    html += `              
+    <div class="card">    
+      <img class="image" src="${result.images[0].src}" alt="${result.name}">
+       <div class="details">
+        <h3 class="name">${result.name}</h3>
+        <p>Price: ${result.prices.price} ${result.prices.price_prefix}</p>                           
+        <a class="btn" href="details.html?id=${result.id}">View More</a>
+      </div>
+  </div>`;
+  });
+
+  flowerContainer.innerHTML = html;
+}
